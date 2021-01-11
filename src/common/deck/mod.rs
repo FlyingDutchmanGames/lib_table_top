bb05394c6f04b615942211b59f456d15fb119d83
Grant Powell <email@grantjamespowell.com>
2021-01-11 01:03:58 -0500

    Add in the concept of a card

--- src/common/deck/mod.rs
+++ src/common/deck/mod.rs
@@ -1,2 +1,12 @@
-mod rank;
-mod suit;
+mod card;
+
+use self::card::rank::*;
+use self::card::suit::*;
+
+pub struct Card(Rank, Suit);
+
+impl Card {
+    pub fn color(&self) -> Color { self.1.color() }
+    pub fn suit(&self) -> Suit { self.1 }
+    pub fn rank(&self) -> Rank { self.0 }
+}
